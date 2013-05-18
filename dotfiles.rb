c28a7106ad4d5a8303134967792df7c5c3e013bb
Stephen Lottermoser <holdtotherod@gmail.com>
2013-05-18 00:02:12 -0700

    dotfiles.rb install working

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # Globals
-$dotfiles_repo_url = "git@github.com:holdtotherod/dotfiles.git"
+$dotfiles_repo_url = "https://github.com/holdtotherod/dotfiles.git"
 $git_repos_dir = "~/repos"
 $filepaths = [
   "~/.profile",
@@ -18,6 +18,7 @@ def install
   if already_installed?
     puts "already installed."
   else
+    puts
     checkout_repo
     remove_existing_files
     symlink_files
@@ -26,11 +27,10 @@ def install
 end
 
 def sync
-  print "Syncing dotfiles..."
+  puts "Syncing dotfiles..."
   `pushd #{dotfiles_repo_path}`
   `git stash`
   `git pull --rebase`
-  `git push`
   `git stash pop`
   `popd`
   `source ~/.profile`
@@ -40,29 +40,34 @@ end
 # Helper methods
 
 def already_installed?
-  File.exists?( File.expand_path repo_path )
+  file_or_symlink_exists? dotfiles_repo_path
 end
 
 def checkout_repo
-  `mkdir #{git_repos_dir}` unless File.exists?( File.expand_path git_repos_dir )
+  `mkdir #{git_repos_dir}` unless file_or_symlink_exists? git_repos_dir 
   `git clone #{dotfiles_repo_url} #{dotfiles_repo_path}`
 end
 
 def remove_existing_files
   $filepaths.each do |filepath|
-    `rm #{filepath}` if File.exists?( File.expand_path filepath )
+    `rm #{filepath}` if file_or_symlink_exists? filepath
   end
 end
 
 def symlink_files
   $filepaths.each do |filepath|
     basename = File.basename filepath
-    `ln -s #{dotfiles_repo_path}/{basename} #{filepath}`
+    `ln -s #{dotfiles_repo_path}/#{basename} #{filepath}`
   end
 end
 
+def file_or_symlink_exists?(filepath)
+  filepath = File.expand_path filepath
+  File.exists?(filepath) || File.symlink?(filepath)
+end
+
 def dotfiles_repo_path
-  repo_foldername = File.basename $repo_url, ".git"
+  repo_foldername = File.basename dotfiles_repo_url, ".git"
   "#{git_repos_dir}/#{repo_foldername}"
 end
 
